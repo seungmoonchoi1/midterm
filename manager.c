8c3f2751c0379a8ac62d828f75c058adc89c108d
seungmoonchoi1 <21800818@handong.ac.kr>
2023-04-24 19:55:12 +0900

    add roaddata

--- manager.c
+++ manager.c
@@ -51,15 +51,25 @@ void saveData(Product p[], int count){
         printf("저장됨!\n");
 }
 //파일에서 데이터 불러오는 함수
-int loadData(Product *p){
-	int count=0;
-	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+int loadData(Product *p){
+        int count=0;
+        FILE*fp;
 
+        //파일 내용을 읽어와서 배열에 값 추가하기
+        fp = fopen("product.txt", "rt");
+        if (fp == NULL) {
+        return 0;
+    }
+    for(; count<100; count++){
+        fscanf(fp, "%s", p[count].name);
+        if(feof(fp)) break;
+        fscanf(fp, "%d", &p[count].weight);
+        fscanf(fp, "%d", &p[count].price);
+    }
 
-
-
-	printf("=> 로딩 성공!\n");
-	return count;
+        fclose(fp);
+        printf("=> 로딩 성공!\n");
+        return count;
 }
